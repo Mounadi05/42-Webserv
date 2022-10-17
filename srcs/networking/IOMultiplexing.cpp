f999859bd49f0e91dc625cdf89dee0a5af63275b
bleedthefreak <tayayassine6@live.fr>
2022-10-17 10:31:53 +0100

    fixing curl error

--- srcs/networking/IOMultiplexing.cpp
+++ srcs/networking/IOMultiplexing.cpp
@@ -83,17 +83,13 @@ void IOMultiplexing::setFdMax(int fd)
 
 void EventLoop(std::vector<Server> &servers, IOMultiplexing &io)
 {
-    // signal(SIGPIPE,SIG_IGN);
     fd_set readcpy, writecpy;
     int fd_client;
     struct timeval vl;
     vl.tv_sec = 3;
     vl.tv_usec = 0;
-    // std::map<Client,Request> ClientRequest;
     std::vector<std::pair<Client, Request> > ClientRequest;
-    std::vector<std::pair<Client, Response> > ClientResponse;
     // std::vector<std::pair<Client, Response> > ClientResponse;
-    // std::vector<std::pair<Client, Response> > ClientRequest;
 
     while (1)
     {
@@ -103,29 +99,28 @@ void EventLoop(std::vector<Server> &servers, IOMultiplexing &io)
         if (r == -1)
         {
             std::cout << "Error in select" << std::endl;
-            break;
         }
         else if (r == 0)
             continue;
         else
         {
-            // int maxfdserver = 0;
             for (size_t j = 0; j < servers.size(); j++)
             {
                 int fdserver = servers[j].getSocket().getSocketFd();
                 if (FD_ISSET(fdserver, &readcpy))
                 {
                     Client newC;
-                    fd_client = accept(fdserver, NULL, NULL); // if fd != -1
-                    newC.setSocketFd(fd_client);
-                    newC.setServer(servers[j]);
-                    ClientRequest.push_back(std::pair<Client, Request>(newC, Request()));
-                    std::cout << "new clinet " << fd_client << " on server " << fdserver << std::endl;
-                    io.setFdRead(fd_client);
-                    if (fd_client > io._fdmax)
-                        io._fdmax = fd_client;
+                    if ((fd_client = accept(fdserver, NULL, NULL)) != -1)
+                    {
+                        newC.setSocketFd(fd_client);
+                        newC.setServer(servers[j]);
+                        ClientRequest.push_back(std::pair<Client, Request>(newC, Request()));
+                        std::cout << "new clinet " << fd_client << " on server " << fdserver << std::endl;
+                        io.setFdRead(fd_client);
+                        if (fd_client > io._fdmax)
+                            io._fdmax = fd_client;
+                    }
                 }
-                // maxfdserver = fdserver;
             }
             for (u_int i = 0; i < ClientRequest.size(); i++)
             {
@@ -138,40 +133,28 @@ void EventLoop(std::vector<Server> &servers, IOMultiplexing &io)
                     {
                         std::cout << "Error in recv" << std::endl;
                         FD_CLR(ClientRequest[i].first.getSocketFd(), &io.fdread);
-                        // FD_CLR(i,&readcpy);
                         close(ClientRequest[i].first.getSocketFd());
-                        // if (std::find(ClientRequest.begin(),ClientRequest.end(),ClientRequest[i]) != ClientRequest.end())
-                        //     ClientRequest.erase(std::find(ClientRequest.begin(),ClientRequest.end(),ClientRequest[i]));
-
+                        ClientRequest.erase(ClientRequest.begin() + i);
                     }
-                    
                     else if (r == 0)
                     {
-                        FD_CLR(ClientRequest[i].first.getSocketFd(), &io.fdread);
                         std::cout << "Client disconnected" << std::endl;
-                        // FD_CLR(i,&readcpy);
+                        FD_CLR(ClientRequest[i].first.getSocketFd(), &io.fdread);
                         close(ClientRequest[i].first.getSocketFd());
+                        ClientRequest.erase(ClientRequest.begin() + i);
                     }
                     else
                     {
                         request[r] = '\0';
-                        
-                        // int finished = readRequest
                         ClientRequest[i].second.setRequestBuffer(request);
-                        
-                        // if (ClientRequest[i].second.getRequestBuffer().find("\n") != std::string::npos ||ClientRequest[i].second.getRequestBuffer().find("\r\n") != std::string::npos )
-                        // {
-                            std::cout << "first line finished" << std::endl;
-                            std::cout << "Request : " << std::endl
-                                    << ClientRequest[i].second.getRequestBuffer() << std::endl;
-                            FD_CLR(ClientRequest[i].first.getSocketFd(), &io.fdread);
-                            FD_SET(ClientRequest[i].first.getSocketFd(), &io.fdwrite);
-                        // }
-                        //if (finished == 1)
-                            // Response newORes(Request, servers);
+                        std::cout << "first line finished" << std::endl;
+                        std::cout << "Request : " << std::endl
+                                  << ClientRequest[i].second.getRequestBuffer() << std::endl;
+                        FD_CLR(ClientRequest[i].first.getSocketFd(), &io.fdread);
+                        FD_SET(ClientRequest[i].first.getSocketFd(), &io.fdwrite);
                     }
                 }
-                if (FD_ISSET(ClientRequest[i].first.getSocketFd(), &writecpy)) // response
+                else if (FD_ISSET(ClientRequest[i].first.getSocketFd(), &writecpy)) // response
                 {
                     std::cout << "i = " << ClientRequest[i].first.getSocketFd() << std::endl;
                     std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 12\r\n\r\nHello World!";
@@ -194,8 +177,8 @@ void EventLoop(std::vector<Server> &servers, IOMultiplexing &io)
     }
 }
 
-Response craft_response(std::pair<Client,Request> ClientRequest)
+Response craft_response(std::pair<Client, Request> ClientRequest)
 {
-    (void) ClientRequest;
+    (void)ClientRequest;
     return Response();
 }
